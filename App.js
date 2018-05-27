d4aee83117580ab9b3af1dc58df4e0f0daf9ebe2
Mariela Rotger <marielarotger@gmail.com>
2018-05-27 13:31:16 -0300

    Primer test simple de javascript

--- App.js
+++ App.js
@@ -1,5 +1,6 @@
 import React, {Component} from 'react';
 import { StyleSheet, Text, View, Button, AppRegistry, Image, TextInput, Alert, ScrollView } from 'react-native';
+import Intro from './src/test/Intro';
 
 class Greeting extends Component {
   constructor(props) {
@@ -42,71 +43,7 @@ export default class App extends React.Component {
     };
 
     return (
-      <ScrollView contentContainerStyle={styles.contentContainer}>
-        
-        <View style={styles.container1}>
-        </View>
-        
-        <View style={styles.container2}>
-          <Text style={styles.bigblue}>Mariano</Text>
-          <Text style={styles.bigblue}>12/12/1984</Text>
-        </View>    
-        
-        <View style={styles.container3}>
-          <Greeting algo=", este es un componente Greeting Creado"/>
-
-          <TextInput
-            style={{fontSize: 20}}
-            placeholder="Escriba su texto aqui"
-          />
-
-          
-          <Button
-            onPress={this.onPressLearnMore}
-            title="HOLA MUNDO"
-            color="black"
-            />
-
-            <Button onPress={() => { Alert.alert( 'You tapped the button!' ); }} 
-            title= "Press Me" 
-            color="white"/>
-        </View>
-
-        <View style={styles.container4}>
-        
-          <Image source={pic} style={{width: 300, height: 200}}/>  
-
-          <View style={styles.container}>
-            <View style={styles.buttonContainer}>
-              <Button
-                onPress={this._onPressButton}
-                title="Press Me"
-              />
-            </View>
-            <View style={styles.buttonContainer}>
-              <Button
-                onPress={this._onPressButton}
-                title="Press Me"
-                color="#841584"
-              />
-            </View>
-            <View style={styles.alternativeLayoutButtonContainer}>
-              <Button
-                onPress={this._onPressButton}
-                title="This looks great!"
-              />
-              <Button
-                onPress={this._onPressButton}
-                title="OK!"
-                color="#841584"
-              />
-            </View>
-          </View>
-
-        </View>
-      
-      </ScrollView>
-    
+      <Intro/>
     );
   }
 }
--- README.md
+++ README.md
@@ -40,3 +40,36 @@ Instalar Expo
 ```
 npm install -g exp
 ```
+
+
+# Test con Jest
+
+snapshots: Es una "imagen" de la pantalla. Lo que esperamos es que cada vez que corremos un test, el nuevo snapshots coincida con el previo. Si la pantalla cambia, el trst falla y hay que actualizar el snapshots si corresponde
+
+```
+// Install dependencies
+npm install --save-dev jest babel-jest jest-react-native babel-preset-react-native react-test-renderer
+
+// Add this to your package.json
+"scripts": {
+  "test": "jest"
+},
+"jest": {
+  "preset": "jest-react-native"
+}
+
+// Add this to your .babelrc
+{
+  "presets": ["react-native"]
+}
+```
+
+Luego para correr los test
+
+```
+npm test
+```
+
+Y corre el test por defecto ``App.test.js``
+
+Creo la carpeta ``src/test`` y los archivos ``sum.js`` y ``sum.test.js``. Vuelvo a correr los test y los toma.
